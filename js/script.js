2b9cdab8c2b890aa532a15c22805b03b0dbace0f
Tony Ramírez <tonyramirez.business@outlook.com>
2023-02-25 18:14:39 -0600

    feat(navbar): add responsive (hamburger menu)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('holaa')
+function navMenu() {
+  let navbar = document.getElementById('myNavbar');
+  let hamburger = document.getElementById('myHamburger')
+
+  console.log(hamburger)
+  if (navbar.tagName === "NAV" && !navbar.className.includes("responsive")) {
+    navbar.className += " responsive";
+    hamburger.className = "fa fa-times";
+  } else {
+    navbar.className -= " responsive";
+    hamburger.className = "fa fa-bars";
+  }
+}
